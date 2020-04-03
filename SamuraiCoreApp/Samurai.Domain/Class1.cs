c2686a383ee39a2e98acfc99183da866083bb1f4
Nick Turner <recbrasil5@gmail.com>
2020-04-02 20:56:31 -0500

    added new core solution

---
+++ SamuraiCoreApp/Samurai.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Samurai.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ SamuraiCoreApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Qutoes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        "Server=localhost\\sqlexpress;Database=SamuraiAppData;Trusted_Connection=True");
+        //}
+    }
+}
